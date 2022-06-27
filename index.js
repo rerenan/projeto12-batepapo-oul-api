28c66ca02047cb5cc93551563efe9386c056ffed
Renan Araujo <ra.carvalho@unesp.br>
2022-06-27 06:56:41 -0300

    sanitização de dados e ajustes em GET mensagens

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express, { json } from 'express';
 import cors from 'cors'
-import {MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 import joi from 'joi';
 import dayjs from 'dayjs';
 import dotenv from 'dotenv';
+import { stripHtml } from 'string-strip-html'
 
 dotenv.config()
 
@@ -16,8 +17,6 @@ let db;
 mongoClient.connect(()=> {
     db = mongoClient.db('batepapo-uol-api');
 })
-
-
 
 const participantSchema = joi.object({
     name: joi.string().required()
@@ -33,8 +32,11 @@ const messageSchema = joi.object({
 setInterval(removeInactiveUsers,15000);
 
 async function removeInactiveUsers(){
+    
     const usersInactives = await db.collection('participants').find({lastStatus:{$lt: Date.now()-10000}}).toArray()
+    
     await db.collection('participants').deleteMany({lastStatus:{$lt: (Date.now()-10000)}})
+    
     usersInactives.forEach( async (user)=>{
         db.collection("messages").insertOne({
             from: user.name,
@@ -56,12 +58,18 @@ app.post('/participants',async (req,res)=>{
         return;
     
     }
+    
     if(nameConflict){
         res.sendStatus(409);
         return;
     }
+    
     try{
-        const participant = {...req.body, lastStatus: Date.now()}
+        const participant = {
+            name: stripHtml(req.body.name).result.trim(), 
+            lastStatus: Date.now()
+        }
+        
         await db.collection('participants').insertOne(participant);
         await db.collection("messages").insertOne({
             from: req.body.name,
@@ -70,7 +78,9 @@ app.post('/participants',async (req,res)=>{
             type: 'status', 
             time: dayjs().format('HH:MM:ss')
         })
+        
         res.sendStatus(201);
+    
     }catch(err){
       console.log(err);
       res.sendStatus(500);  
@@ -82,6 +92,7 @@ app.get('/participants', async (req,res)=>{
     try{
         const participants = await db.collection('participants').find().toArray();
         res.send(participants);
+    
     }catch(err){
         console.log(err)
         res.sendStatus(500)
@@ -89,30 +100,54 @@ app.get('/participants', async (req,res)=>{
 })
 
 app.post('/messages', async (req,res)=>{
+    
     const validation = messageSchema.validate(req.body,{abortEarly: false})
     const userVerify = await db.collection('participants').findOne({name: req.headers.user})
+    
     if(validation.error){
         res.status(422).send(validation.error.details.map((item)=>item.message))
         return;
     }
+    
     if(!userVerify){
         res.status(422).send("User Disconnected, please reload page.")
         return;
     }
+    
     try {
-        const message = {...req.body, from: req.headers.user, time:dayjs().format('HH:MM:ss')}
+        const message = {
+            ...req.body,
+            text: stripHtml(req.body.text).result.trim(),
+            from: req.headers.user,
+            time:dayjs().format('HH:MM:ss')
+        }
+
         await db.collection('messages').insertOne(message);
         res.sendStatus(201)
-    } catch (err) {
+
+    }catch (err) {
         console.log(err)
         res.sendStatus(500)
     }
 })
 
 app.get('/messages', async (req,res)=>{
+    const limitMessages = req.query.limit
+    const user = req.headers.user
     try{
-        const messages = await db.collection('messages').find().toArray();
-        res.send(messages);
+        const messages = await db.collection('messages').find(
+            {
+                $or:[
+                {from: user},
+                {type:"message"},
+                {type:"status"},
+                {to: user, type: "private_message"}
+                ]
+            }
+        ).toArray();
+
+        res.send(messages.slice(-limitMessages));
+
     }catch(err){
         console.log(err)
         res.sendStatus(500)
@@ -120,21 +155,23 @@ app.get('/messages', async (req,res)=>{
 })
 
 app.post('/status', async (req,res)=>{
+    
     const user = req.headers.user;
-    const userVerify = await db.collection('participants').findOne({name: user})
+    const userVerify = await db.collection('participants').findOne({name: stripHtml(user).result})
+    
     if(!userVerify){
         res.sendStatus(404);
         return;
     }
+    
     try{
         await db.collection('participants').updateOne({name: user},{$set: {lastStatus: Date.now()}})
         res.sendStatus(200);
+    
     }catch(err){
         console.log(err)
         res.sendStatus(500)
     }
-    
-
 })
 
 app.listen(5000);
